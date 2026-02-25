fc3547a1021e57a8e930053617be525144bac720
jjoud <Joud1042@hotmail.com>
2026-02-25 06:15:41 +0300

    lab4

--- apps/bookmodule/apps.py
+++ apps/bookmodule/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class BookmoduleConfig(AppConfig):
     name = 'apps.bookmodule'
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -21,7 +21,7 @@ import apps.bookmodule.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', apps.bookmodule.views.index),
-   #path('index2/<int:val1>/', apps.bookmodule.views.index2),
+    #path('index2/<int:val1>/', apps.bookmodule.views.index2),
     path('books/', include("apps.bookmodule.urls")),
     path('users/', include("apps.usermodule.urls")),
 
